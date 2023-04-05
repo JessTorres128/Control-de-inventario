c4ea1089deb58ea3cf8b17919ad7ee3311561f33
JessTorres128 <jesseatorres128@gmail.com>
2023-04-04 17:56:59 -0700

    Login

--- src/main/java/com/example/controldeinventario/HelloApplication.java
+++ src/main/java/com/example/controldeinventario/HelloApplication.java
@@ -8,13 +8,16 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    public static Stage primarystage;
     @Override
     public void start(Stage stage) throws IOException {
+        primarystage =stage;
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 350, 350);
         stage.setTitle("Inventario");
         stage.setScene(scene);
         stage.show();
+        stage.setResizable(false);
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/controldeinventario/LoginController.java
+++ src/main/java/com/example/controldeinventario/LoginController.java
@@ -1,14 +1,47 @@
 package com.example.controldeinventario;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class LoginController {
-    @FXML
-    private Label welcomeText;
+@FXML TextField txtuser;
+@FXML TextField txtpassword;
+Conexion conexion;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Cambios");
+@FXML private void IngresarLogin(ActionEvent event) throws IOException, SQLException {
+    String user = txtuser.getText();
+    String pass = txtpassword.getText();
+
+    ResultSet resultado = conexion.consultar("select * from usuarios where username='"+user+"' and password='"+pass+"'");
+    if (resultado != null){
+        int cont =0;
+        if (resultado.next()){cont++;}
+
+        if(cont==0){
+            Alert alert=new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("error");
+            alert.setContentText("Datos incorrectos");
+            alert.show();
+        }else{
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("principal.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 550, 400);
+            HelloApplication.primarystage.setX(100);
+            HelloApplication.primarystage.setY(50);
+            HelloApplication.primarystage.setTitle("Pantalla principal");
+            HelloApplication.primarystage.setScene(scene);
+            HelloApplication.primarystage.setResizable(false);
+        }
+
     }
+}
+
 }
